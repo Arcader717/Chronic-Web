8cf0d0851001235bf52d2db07fd3cae4198aaedf
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-11-15 13:34:58 +0000

    Update app.py hypercorn_config.py and requirements

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Quart(__name__)
 @app.route('/')
 async def index():
     return "Testing Out AWS"
+
+if __name__ == "__main__":
+    import hypercorn
+    from hypercorn.asyncio import serve
+    from hypercorn.config import config
+
+    config = Config.from_path("hypercorn_config.py")
+    serve(app, config)
