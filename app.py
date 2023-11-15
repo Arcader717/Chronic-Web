cc21458f6b2c52333e7aefed5672e4126d8fd1ee
Arcader717 <cwterry2009@gmail.com>
2023-11-15 16:58:19 +0000

    Adding some functions from discoauth, won't be permanent

--- app.py
+++ app.py
@@ -3,10 +3,16 @@
 
 from flask import Flask, render_template
 
+from chronic.common import generate_token
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/')
 def index():
     return render_template("main_page.html")
+
+@app.route('/login')
+def login():
+    return f"{generate_token()}"
 
