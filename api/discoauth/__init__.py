559762e292a29fdfa1952d0395ba8e1fe1647dbc
Arcader717 <134526462+Arcader717@users.noreply.github.com>
2023-12-23 15:20:46 -0600

    Rename discoauth/__init__.py to api/discoauth/__init__.py

---
+++ api/discoauth/__init__.py
@@ -0,0 +1,3 @@
+from .url import auth, discord, bot, permissions
+
+from .__version__ import __title__, __version__, __description__, __author__
--- discoauth/__init__.py
+++
@@ -1,3 +0,0 @@
-from .url import auth, discord, bot, permissions
-
-from .__version__ import __title__, __version__, __description__, __author__
